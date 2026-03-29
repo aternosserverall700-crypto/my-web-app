1422310ccbed8c5879bc31b79e95f535f4be494c
aternosserverall700-crypto <aternos.server.all.700@gmail.com>
2026-03-29 13:20:45 +0100

    sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,18 @@
-const cacheName = 'belrhazi-v1';
+const cacheName = 'belrhazi-v2';
 const assets = [
   './',
-  './index.html'
+  './index.html',
+  'https://cdn.tailwindcss.com' // هادي ضرورية باش الألوان والتصميم يبقاو خدامين بلا انترنيت
 ];
 
 self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open(cacheName).then(cache => {
-      return cache.addAll(assets);
-    })
+    caches.open(cacheName).then(cache => cache.addAll(assets))
   );
 });
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(res => {
-      return res || fetch(e.request);
-    })
+    caches.match(e.request).then(res => res || fetch(e.request))
   );
 });
